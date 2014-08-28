3a34c7ce33a26c4f1cc078ce61dcd5954eb1cb55
shahidulhasan <er.shahidul@gmail.com>
2014-08-28 16:27:37 +0600

    28.08.2014-2

--- src/Pms/CoreBundle/Controller/CoreController.php
+++ src/Pms/CoreBundle/Controller/CoreController.php
@@ -5,12 +5,14 @@ namespace Pms\CoreBundle\Controller;
 use Doctrine\ORM\Repository;
 use Pms\CoreBundle\Entity\Category;
 use Pms\CoreBundle\Entity\Buyer;
+use Pms\CoreBundle\Entity\Vendor;
 use Pms\CoreBundle\Entity\Item;
 use Pms\CoreBundle\Entity\Project;
 use Pms\CoreBundle\Entity\ProjectCostItem;
 use Pms\CoreBundle\Entity\PurchaseRequisition;
 use Pms\CoreBundle\Form\CategoryType;
 use Pms\CoreBundle\Form\BuyerType;
+use Pms\CoreBundle\Form\VendorType;
 use Pms\CoreBundle\Form\ItemType;
 use Pms\CoreBundle\Form\ProjectCostItemType;
 use Pms\CoreBundle\Form\ProjectType;
@@ -23,6 +25,174 @@ use Symfony\Component\HttpFoundation\Response;
 
 class CoreController extends Controller
 {
+    public function vendorAddAction(Request $request)
+    {
+        $vendor = new Vendor();
+
+        $form = $this->createForm(new VendorType(), $vendor);
+
+        $dql = "SELECT a FROM PmsCoreBundle:Vendor a WHERE a.status = 1 ORDER BY a.id DESC";
+
+        list($vendors, $page) = $this->paginate($dql);
+
+        return $this->render('PmsCoreBundle:Vendor:add.html.twig', array(
+            'vendors' => $vendors,
+            'form' => $form->createView(),
+            'page' => $page,
+        ));
+    }
+
+    public function vendorDeletedAction(Request $request)
+    {
+        $vendor = new Vendor();
+
+        $form = $this->createForm(new VendorType(), $vendor);
+
+        $dql = "SELECT a FROM PmsCoreBundle:Vendor a WHERE a.status = 0 ORDER BY a.id DESC";
+
+        list($vendors, $page) = $this->paginate($dql);
+
+        return $this->render('PmsCoreBundle:Vendor:deleted.html.twig', array(
+            'vendors' => $vendors,
+            'form' => $form->createView(),
+            'page' => $page,
+        ));
+    }
+
+    public function vendorListAction()
+    {
+        $dql = "SELECT a FROM PmsCoreBundle:Vendor a WHERE a.status = 1 ORDER BY a.id DESC";
+
+        list($vendors, $page) = $this->paginate($dql);
+
+        return $this->render('PmsCoreBundle:Vendor:list.html.twig', array(
+            'vendors' => $vendors,
+            'page' => $page,
+        ));
+    }
+
+    public function vendorDeletedListAction()
+    {
+        $dql = "SELECT a FROM PmsCoreBundle:Vendor a WHERE a.status = 0 ORDER BY a.id DESC";
+
+        list($vendors, $page) = $this->paginate($dql);
+
+        return $this->render('PmsCoreBundle:Vendor:deletedList.html.twig', array(
+            'vendors' => $vendors,
+            'page' => $page,
+        ));
+    }
+
+    public function vendorDeleteAction(Vendor $vendor)
+    {
+        $vendor->setStatus(0);
+        $this->getDoctrine()->getRepository('PmsCoreBundle:Vendor')->update($vendor);
+
+        $this->get('session')->getFlashBag()->add(
+            'notice',
+            'Vendor Successfully Deleted'
+        );
+
+        return $this->redirect($this->generateUrl('vendor_add'));
+    }
+
+    public function vendorActiveAction(Vendor $vendor)
+    {
+        $vendor->setStatus(1);
+        $this->getDoctrine()->getRepository('PmsCoreBundle:Vendor')->update($vendor);
+
+        $this->get('session')->getFlashBag()->add(
+            'notice',
+            'Vendor Successfully Restored'
+        );
+
+        return $this->redirect($this->generateUrl('vendor_deleted'));
+    }
+
+    public function vendorAjaxAddAction(Request $request)
+    {
+        $vendorArray = $request->request->get('vendorArray');
+        $vendorArray = explode(',',$vendorArray);
+
+        $vendorName = $vendorArray[0];
+        $updateId = $vendorArray[1];
+
+        if($vendorName) {
+            $vendor = $this->getDoctrine()->getRepository('PmsCoreBundle:Vendor')->find($updateId);
+            $vendorNameCheck = $this->getDoctrine()->getRepository('PmsCoreBundle:Vendor')->findOneBy(
+                array('vendorName' => $vendorName )
+            );
+            if($vendor) {
+                $vendor->setVendorName($vendorName);
+                $this->getDoctrine()->getManager()->persist($vendor);
+                $this->getDoctrine()->getManager()->flush();
+
+                $return = array("responseCode" => 202);
+                $return = json_encode($return);
+
+                return new Response($return, 200, array('Content-Type' => 'application/json'));
+            } elseif($vendorNameCheck) {
+                $return = array("responseCode" => 200);
+                $return = json_encode($return);
+
+                return new Response($return, 200, array('Content-Type' => 'application/json'));
+            } else {
+                $vendor = new Vendor();
+                $vendor->setVendorName($vendorName);
+                $user = $this->get('security.context')->getToken()->getUser()->getId();
+                $vendor->setCreatedBy($user);
+                $vendor->setCreatedDate(new \DateTime());
+                $vendor->setStatus(1);
+
+                $this->getDoctrine()->getRepository("PmsCoreBundle:Vendor")->create($vendor);
+
+                $return = array("responseCode" => 404);
+                $return = json_encode($return);
+
+                return new Response($return, 200, array('Content-Type' => 'application/json'));
+            }
+        } else{
+            $return = array("responseCode" => 204);
+            $return = json_encode($return);
+
+            return new Response($return, 200, array('Content-Type' => 'application/json'));
+        }
+    }
+
+    public function vendorCheckAction(Request $request)
+    {
+        $vendorName = $request->request->get('vendorName');
+
+        $vendor = $this->getDoctrine()->getRepository('PmsCoreBundle:Vendor')->findOneBy(
+            array('vendorName' => $vendorName )
+        );
+
+        if ($vendor) {
+            $return = array("responseCode" => 200, "vendor_name" => "Vendor already exist.");
+            $return = json_encode($return);
+            return new Response($return, 200, array('Content-Type' => 'application/json'));
+        } else {
+            $return = array("responseCode" => '404', "vendor_name" => "Vendor name available.");
+            $return = json_encode($return);
+            return new Response($return, 200, array('Content-Type' => 'application/json'));
+        }
+    }
+
+    public function vendorUpdateAction(Request $request, Vendor $vendor)
+    {
+        $form = $this->createForm(new VendorType(), $vendor);
+
+        $dql = "SELECT a FROM PmsCoreBundle:Vendor a WHERE a.status = 1 ORDER BY a.id DESC";
+
+        list($vendors, $page) = $this->paginate($dql);
+
+        return $this->render('PmsCoreBundle:Vendor:add.html.twig', array(
+            'vendors' => $vendors,
+            'form' => $form->createView(),
+            'page' => $page,
+        ));
+    }
+
     public function purchaseOrderedAddAction(Request $request)
     {
         return $this->render('PmsCoreBundle:PurchaseOrdered:add.html.twig', array(
---
+++ src/Pms/CoreBundle/Entity/Vendor.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Pms\CoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Vendor
+ *
+ * @ORM\Table(name="vendors")
+ * @ORM\Entity(repositoryClass="Pms\CoreBundle\Entity\VendorRepository")
+ */
+class Vendor
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="vendors_name", type="string", length=255)
+     */
+    private $vendorName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="created_by", type="string", length=255)
+     */
+    private $createdBy;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_date", type="datetime")
+     */
+    private $createdDate;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="status", type="integer")
+     */
+    private $status;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    /**
+     * Set vendorName
+     *
+     * @param string $vendorName
+     * @return Vendor
+     */
+    public function setVendorName($vendorName)
+    {
+        $this->vendorName = $vendorName;
+
+        return $this;
+    }
+
+    /**
+     * Get vendorName
+     *
+     * @return string
+     */
+    public function getVendorName()
+    {
+        return $this->vendorName;
+    }
+
+    /**
+     * Set createdBy
+     *
+     * @param string $createdBy
+     * @return Vendor
+     */
+    public function setCreatedBy($createdBy)
+    {
+        $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    /**
+     * Get createdBy
+     *
+     * @return string
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * Set createdDate
+     *
+     * @param \DateTime $createdDate
+     * @return Vendor
+     */
+    public function setCreatedDate($createdDate)
+    {
+        $this->createdDate = $createdDate;
+
+        return $this;
+    }
+
+    /**
+     * Get createdDate
+     *
+     * @return \DateTime
+     */
+    public function getCreatedDate()
+    {
+        return $this->createdDate;
+    }
+
+    /**
+     * Set status
+     *
+     * @param integer $status
+     * @return Vendor
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return integer
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+}
