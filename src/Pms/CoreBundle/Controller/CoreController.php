6f24ba3d07fe85cb8c0fdaae0bac4102bac9bf48
shahidulhasan <er.shahidul@gmail.com>
2014-08-31 16:15:07 +0600

    31.08.2014-5

--- src/Pms/CoreBundle/Controller/CoreController.php
+++ src/Pms/CoreBundle/Controller/CoreController.php
@@ -56,20 +56,18 @@ class CoreController extends Controller
 
     public function purchaseRequisitionAddAction(Request $request)
     {
-        return $this->render('PmsCoreBundle:PurchaseRequisition:add.html.twig', array(
+        $dql = "SELECT a FROM PmsCoreBundle:PurchaseRequisition a WHERE a.status = 1 ORDER BY a.id DESC";
 
+        list($purchaseRequisitions, $page) = $this->paginate($dql);
+
+        return $this->render('PmsCoreBundle:PurchaseRequisition:add.html.twig', array(
+            'purchaseRequisitions' => $purchaseRequisitions,
+            'page' => $page,
         ));
     }
 
     public function purchaseRequisitionNewAction(Request $request)
     {
-
-        $ri1 = new PurchaseRequisitionItem();
-        $ri1->setQuantity(44);
-
-        $ri2 = new PurchaseRequisitionItem();
-        $ri2->setQuantity(44);
-
         $purchaseRequisition = new PurchaseRequisition();
         $form = $this->createForm(new PurchaseRequisitionType(), $purchaseRequisition);
 
@@ -113,207 +111,6 @@ class CoreController extends Controller
         ));
     }
 
-//    public function prAjaxAddAction(Request $request)
-//    {
-//        $prArray = $request->request->get('prArray');
-//        $prArray = explode(',',$prArray);
-//
-//        $project = $prArray[0];
-//        $requisitionNo = $prArray[1];
-//        $dateOfRequisition = $prArray[2];
-//        $item = $prArray[3];
-//        $quantity = $prArray[4];
-//        $dateOfRequired = $prArray[5];
-//        $comment = $prArray[6];
-//
-//        if(!empty($project) && !empty($requisitionNo) && !empty($dateOfRequisition) && !empty($item) && !empty($quantity) && !empty($dateOfRequired) && !empty($comment)) {
-//
-//                $purchaseRequisition = new PurchaseRequisition();
-//
-//                $user = $this->get('security.context')->getToken()->getUser()->getId();
-//            $purchaseRequisition->setDateOfRequisition(new \DateTime($dateOfRequisition));
-//            $purchaseRequisition->setCreatedBy($user);
-//            $purchaseRequisition->setCreatedDate(new \DateTime());
-//            $purchaseRequisition->setStatus(1);
-//            $purchaseRequisition->setRequisitionNo($requisitionNo);
-//            $purchaseRequisition->setProject($this->getDoctrine()->getRepository('PmsCoreBundle:Project')->findOneById($project));
-//
-//
-//            $purchaseRequisition->setItem($this->getDoctrine()->getRepository('PmsCoreBundle:PurchaseRequisitionItem')->findOneById($item));
-//            $purchaseRequisition->setQuantity($this->getDoctrine()->getRepository('PmsCoreBundle:PurchaseRequisitionItem')->findOneById($quantity));
-//            $purchaseRequisition->setDateOfRequired($this->getDoctrine()->getRepository('PmsCoreBundle:PurchaseRequisitionItem')->findOneById(new \DateTime($dateOfRequired)));
-//            $purchaseRequisition->setComment($this->getDoctrine()->getRepository('PmsCoreBundle:PurchaseRequisitionItem')->findOneById($comment));
-//
-//
-//                $this->getDoctrine()->getRepository("PmsCoreBundle:ProjectCostItem")->create($purchaseRequisition);
-//
-//                $return = array("responseCode" => '404');
-//                $return = json_encode($return);
-//
-//                return new Response($return, 200, array('Content-Type' => 'application/json'));
-//
-//        } else{
-//            $return = array("responseCode" => 204);
-//            $return = json_encode($return);
-//
-//            return new Response($return, 200, array('Content-Type' => 'application/json'));
-//        }
-//    }
-
-//    public function overViewAction(Request $request)
-//    {
-//        $em = $this->getDoctrine()->getManager();
-//
-//        if(!empty($_GET['start_date']) && !empty($_GET['end_date'])){
-//
-//            $start = $_GET['start_date'];
-//            $end = $_GET['end_date'];
-//        }else{
-//
-//            $start = 0;
-//            $end = 0;
-//        }
-//
-//        $itemUses = $this->getDoctrine()->getRepository('PmsCoreBundle:ProjectCostItem')->overView($em, $start, $end);
-//
-//        list($itemUses, $page) = $this->paginateOverView($itemUses);
-//
-//        $formSearch = $this->createForm(new SearchType());
-//
-//        return $this->render('PmsCoreBundle:Report:over_view.html.twig', array(
-//            'start' => $start,
-//            'end' => $end,
-//            'itemUses' => $itemUses,
-//            'page' => $page,
-//            'formSearch' => $formSearch->createView(),
-//        ));
-//    }
-//
-//    public function itemReportAction()
-//    {
-//        $em = $this->getDoctrine()->getManager();
-//
-//        if(!empty($_GET['start_date']) && !empty($_GET['end_date'])){
-//
-//            $startDate = $_GET['start_date'];
-//            $endDate = $_GET['end_date'];
-//        }else{
-//
-//            $startDate = 0;
-//            $endDate = 0;
-//        }
-//
-//        list($itemUses, $itemTotal, $itemsPieChartData, $sumOfTopTen, $totalItemForPie, $totalSum, $categoriesPieChartData, $totalCategoryForPic) = $this->getDoctrine()->getRepository('PmsCoreBundle:ProjectCostItem')->itemReport($em, $startDate, $endDate);
-//
-//        $formSearch = $this->createForm(new SearchType());
-//
-//        return $this->render('PmsCoreBundle:Report:item.html.twig', array(
-//            'startDate' => $startDate,
-//            'endDate' => $endDate,
-//            'itemUses' => $itemUses,
-//            'itemTotal' => $itemTotal,
-//            'sumOfTopTen' => $sumOfTopTen,
-//            'itemsPieChartData' => $itemsPieChartData,
-//            'categoriesPieChartData' => $categoriesPieChartData,
-//            'formSearch' => $formSearch->createView(),
-//            'totalItemForPie' => $totalItemForPie,
-//            'totalSum' => $totalSum,
-//            'totalCategoryForPic' => $totalCategoryForPic,
-//        ));
-//    }
-//
-//    public function itemDetailsAction($id, $start, $end)
-//    {
-//        $em = $this->getDoctrine()->getManager();
-//
-//        list($itemUses, $itemTotal, $reportData) = $this->getDoctrine()->getRepository('PmsCoreBundle:ProjectCostItem')->itemDetails($id, $em, $start, $end);
-//
-//        return $this->render('PmsCoreBundle:Report:item_details.html.twig', array(
-//            'start' => $start,
-//            'end' => $end,
-//            'itemUses' => $itemUses,
-//            'itemTotal' => $itemTotal,
-//            'reportData' => $reportData,
-//        ));
-//    }
-//
-//    public function byItemDetailsAction($id, $start, $end, $project)
-//    {
-//        $em = $this->getDoctrine()->getManager();
-//
-//        list($itemUses, $itemTotal, $reportData) = $this->getDoctrine()->getRepository('PmsCoreBundle:ProjectCostItem')->byItemDetails($id, $em, $start, $end, $project);
-//
-//        return $this->render('PmsCoreBundle:Report:by_item_details.html.twig', array(
-//            'start' => $start,
-//            'end' => $end,
-//            'itemUses' => $itemUses,
-//            'itemTotal' => $itemTotal,
-//            'reportData' => $reportData,
-//            'project' => $project,
-//        ));
-//    }
-//
-//    public function byProjectDetailsAction($id, $startDate, $endDate)
-//    {
-//        $em = $this->getDoctrine()->getManager();
-//
-//        list($projectItems, $itemsTotal, $itemsPieChartData, $categoriesPieChartData) = $this->getDoctrine()->getRepository('PmsCoreBundle:ProjectCostItem')->projectDetails($id, $em, $startDate, $endDate);
-//
-//        return $this->render('PmsCoreBundle:Report:by_project_details.html.twig', array(
-//            'categoriesPieChartData' => $categoriesPieChartData,
-//            'itemsPieChartData' => $itemsPieChartData,
-//            'projectItems' => $projectItems,
-//            'itemsTotal' => $itemsTotal,
-//            'startDate' => $startDate,
-//            'endDate' => $endDate,
-//        ));
-//    }
-//
-//    public function projectReportAction(Request $request)
-//    {
-//        $em = $this->getDoctrine()->getManager();
-//
-//        if(!empty($_GET['start_date']) && !empty($_GET['end_date'])){
-//
-//            $startDate = $_GET['start_date'];
-//            $endDate = $_GET['end_date'];
-//        }else{
-//
-//            $startDate = 0;
-//            $endDate = 0;
-//        }
-//
-//        list($projectCostItems, $totalCost, $projectPieChartData, $sumOfTopTen, $totalProjectForPic, $categoryPieChartData, $totalCategoryForPic, $totalSum) = $this->getDoctrine()->getRepository('PmsCoreBundle:ProjectCostItem')->projectReport($em, $startDate, $endDate);
-//
-//        $formSearch = $this->createForm(new SearchType());
-//
-//        return $this->render('PmsCoreBundle:Report:project.html.twig', array(
-//            'categoryPieChartData' => $categoryPieChartData,
-//            'projectPieChartData' => $projectPieChartData,
-//            'totalCategoryForPic' => $totalCategoryForPic,
-//            'totalProjectForPic' => $totalProjectForPic,
-//            'formSearch' => $formSearch->createView(),
-//            'projectCostItems' => $projectCostItems,
-//            'sumOfTopTen' => $sumOfTopTen,
-//            'totalCost' => $totalCost,
-//            'totalSum' => $totalSum,
-//            'startDate' => $startDate,
-//            'endDate' => $endDate,
-//        ));
-//    }
-
-//    public function paginateOverView($itemUses)
-//    {
-//        $paginator = $this->get('knp_paginator');
-//        $value = $paginator->paginate(
-//            $itemUses,
-//            $page = $this->get('request')->query->get('page', 1) /*page number*/,
-//            10/*limit per page*/
-//        );
-//
-//        return array($value, $page);
-//    }
-
     public function paginate($dql)
     {
         $em = $this->get('doctrine.orm.entity_manager');
@@ -323,7 +120,7 @@ class CoreController extends Controller
         $value = $paginator->paginate(
             $query,
             $page = $this->get('request')->query->get('page', 1) /*page number*/,
-            10/*limit per page*/
+            50/*limit per page*/
         );
 
         return array($value, $page);
